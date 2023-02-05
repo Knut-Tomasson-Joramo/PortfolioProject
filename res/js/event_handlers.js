cd297c0abe060374a583f1c7c77da78c0440e2ee
Knut Tomasson Joramo <knut.joramo@gmail.com>
2023-02-06 00:11:51 +0100

    add show button behavior

--- res/js/event_handlers.js
+++ res/js/event_handlers.js
@@ -3,3 +3,21 @@
  */
 
 // get array of all the buttons
+const show_buttons = document.getElementsByClassName("show-project");
+
+
+for (let i = 0; i < show_buttons.length; i++) {
+    const button = show_buttons[i];
+    button.addEventListener('click', function(target) {
+      let project_section = target.currentTarget.parentNode.parentNode;
+   
+      if (project_section.style.height === "") {
+        console.log(target.currentTarget);
+        project_section.style.height = "fit-content";
+      }
+      else if (project_section.style.height === "fit-content") {
+         project_section.style.height = "";
+        }
+      
+    });
+}
